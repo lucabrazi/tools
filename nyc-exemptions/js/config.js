8108ee719c4bbaec29931deee1f84fb17b75b091
Lucabrazi <david.raciti@gmail.com>
2025-08-15 14:56:00 -0400

    Remove config.js and add to .gitignore
    Deleted js/config.js containing sensitive configuration and added it to .gitignore to prevent future commits of this file.

--- nyc-exemptions/js/config.js
+++
@@ -1,7 +0,0 @@
-// Public token for read-only datasets. Safe to embed when necessary.
-// Keep this file out of your repo.
-const CONFIG = {
-  APP_TOKEN: "fbbsJ6S1wwtWafNbQAEpKqMTX",
-};
-
-export default CONFIG;
